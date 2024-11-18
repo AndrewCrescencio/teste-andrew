e501175584756bbdfd71e0422b6cee30c0e36ef3
AndrewCrescencio <andrew_workstation@hotmail.com>
2024-11-18 15:22:07 -0300

    feat: add and configure cypress component testing

---
+++ cypress.config.ts
@@ -0,0 +1,11 @@
+// eslint-disable-next-line ts/no-require-imports
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  component: {
+    devServer: {
+      framework: 'vue',
+      bundler: 'vite',
+    },
+  },
+})
--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,13 @@
 // @ts-check
 import antfu from '@antfu/eslint-config'
+import pluginCypress from 'eslint-plugin-cypress/flat'
 
 export default antfu({
   unocss: true,
   formatters: true,
+  plugins: {
+    cypress: pluginCypress,
+
+  },
+  ...pluginCypress.configs.globals,
 })
